fccc2c61fac9b191e6d6526257bd954e8ce4bad9
Masanori Ohgita <mp@ohgita.info>
2013-06-03 08:52:35 +0900

    Fix

--- lib/Net/OECU/LAN/MC2Wifi.pm
+++ lib/Net/OECU/LAN/MC2Wifi.pm
@@ -22,8 +22,44 @@ sub new {
 	$self->{ua} = LWP::UserAgent->new;
 	$self->{ua}->timeout(10);
 	$self->{ua}->max_redirect(5);
-	
-	#$ENV{HTTPS_CA_FILE} = '.webauth.it.osakac.ac.jp_chain.cert';
+
+	my $a = <<'EOF';
+-----BEGIN CERTIFICATE-----
+MIIEvzCCA6egAwIBAgISESHNMfeNJw/6mnbdn/53ZgNCMA0GCSqGSIb3DQEBBQUA
+MC4xETAPBgNVBAoTCEFscGhhU1NMMRkwFwYDVQQDExBBbHBoYVNTTCBDQSAtIEcy
+MB4XDTEzMDMyOTA0NTcyN1oXDTE2MDMyOTA0NTcyN1owTTEhMB8GA1UECxMYRG9t
+YWluIENvbnRyb2wgVmFsaWRhdGVkMSgwJgYDVQQDEx9tY3dsY3Qxcy5tYzJlZC5z
+am4ub3Nha2FjLmFjLmpwMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA
+xDuCZ58nRR9L2H8wHr2WRbtTJtttFANowCSc3Lg9cgYi6GQPMqYSwPjaYU/Hyq4O
+ACvol8OqytLu4+MSK6Q8Gd9BkkGj/xGHZ6/d7b3qm94XrvrC7+QU9/YEUh7V+Obc
+0dUhBITrfbd82BdyB++chtij32doTespT4sT9+XG2cfGjo9SD6x2KNll94WIJZ2C
+OfKBjPpLLKkd4kHX6nb45nBENlFlQidCJGs1gqxPPOut9z6uoVV/had6C7lUq2rU
+jP2kBv20vBi0VdJFbfbRm4GChndyE2fFfQMD4HFGhEjtVAvI0o2dB5g5Fg4UlnPj
+0asulGguir1AMCXtOMYepwIDAQABo4IBtjCCAbIwDgYDVR0PAQH/BAQDAgWgME0G
+A1UdIARGMEQwQgYKKwYBBAGgMgEKCjA0MDIGCCsGAQUFBwIBFiZodHRwczovL3d3
+dy5nbG9iYWxzaWduLmNvbS9yZXBvc2l0b3J5LzAqBgNVHREEIzAhgh9tY3dsY3Qx
+cy5tYzJlZC5zam4ub3Nha2FjLmFjLmpwMAkGA1UdEwQCMAAwHQYDVR0lBBYwFAYI
+KwYBBQUHAwEGCCsGAQUFBwMCMDoGA1UdHwQzMDEwL6AtoCuGKWh0dHA6Ly9jcmwy
+LmFscGhhc3NsLmNvbS9ncy9nc2FscGhhZzIuY3JsMH8GCCsGAQUFBwEBBHMwcTA8
+BggrBgEFBQcwAoYwaHR0cDovL3NlY3VyZTIuYWxwaGFzc2wuY29tL2NhY2VydC9n
+c2FscGhhZzIuY3J0MDEGCCsGAQUFBzABhiVodHRwOi8vb2NzcDIuZ2xvYmFsc2ln
+bi5jb20vZ3NhbHBoYWcyMB0GA1UdDgQWBBRCGkSrdQsUmmW38sBfhTAUFUpFZDAf
+BgNVHSMEGDAWgBQU6hlV8A4NMsYfdDO3jmYaTBIxHjANBgkqhkiG9w0BAQUFAAOC
+AQEAHFVNx4lw8yZW+/ybZS/yv07GcZO0q5yzr9mEDTu7fqnvSL/Ojli+NghAw8yX
+zNC/YToHy+Wi/BgLb3WzrnzAPWORA9NbsEnwAAJwpVPMhsu9+Btk+jRuQdGjCL7G
+bthgywBj+FLbFAs7JZGP4o/u6f4GucLImyCBcuz32+rMRRRv9i6EJkMZjXFUXADK
+bXHQ3ZGmF15+OUAum7e6v8R2MEytwHigYGL1ZhI0Jj3AvPCvBpd90udp4l9ZSjTf
+z7ePYpdQlzckT53FmIArzNcunTDMnzVM9y1dNX9An5B3Sb5Gi8bxRWNzC+WyvBRE
+kCWjnNAM/0HrGgJ1m7TkmSwsLA==
+-----END CERTIFICATE-----
+EOF
+
+	$self->{ua}->ssl_opts(
+		verify_hostname => 1,
+		SSL_cert => {
+			"mcwlct1s.mc2ed.sjn.osakac.ac.jp" => $a,
+		},
+	);
 	
 	return $self;
 }
@@ -67,14 +103,17 @@ sub login {
 		'password' => $self->{password},
 		'submit' => 'Login',
 	);
-
-	print $response->as_string."\n\n";
 	
-	if ($response->is_success) {
-		print $response->decoded_content;
-		return 1;		
+	unless ($response->is_success) {
+		warn '[Debug] '.$response->as_string."\n";
+		die '[Error] Login error : '.$response->status_line."\n";
 	}
-	die $response->status_line;
+
+	if($response->as_string =~ /window\.location\.href\=\'(http\:\/\/[^.]+)\'/){
+		print "$1\n";
+	}
+
+	return 1;
 }
 
 # logout() - Logout from Wlannet
@@ -97,6 +136,18 @@ sub is_logged_in {
 		return 0;
 	}
 	return 1;
+}
+
+# env_set_proxy() - Set the proxy to ENV
+sub env_set_proxy {
+	my $self = shift;
+
+}
+
+# env_set_proxy() - Unset the proxy to ENV
+sub env_unset_proxy {
+	my $self = shift;
+
 }
 
 # _ua_set_proxy
--- odenwlan.pl
+++ odenwlan.pl
@@ -1,25 +1,67 @@
+#!/usr/bin/env perl
+
+###################################
+# OdenWlan perl
+# https://github.com/odentools/oden_wlan_perl
+# (C) OdenTools Project - 2013.
+###################################
 
 use lib 'lib/';
 use Net::OECU::LAN::MC2Wifi;
 use Config::Pit;
 
-my $config = Config::Pit->piy_get("odenwlan");
+# Initialize and read configurations. (ARGV parameter is primary.)
+my $user = {
+	'username' => '',
+	'password' => '',
+};
 
-my $network = Net::OECU::LAN::MC2Wifi->new(
-	username => '',
-	password => '',
-);
+my $config;
+eval {
+	$config = Config::Pit::get("odenwlan");
+	print Dumper $config;
+	if(defined $config->{username}){
+		$user->{username} = $config->{username};
+	}
+	if(defined $config->{password}){
+		$user->{password} = $config->{password};
+	}
+}; if ($@) { warn "[Warn] $@\n"; }
 
-
-if($network->is_logged_in()){
-	print "[Info] すでにログイン済みです。\n";
-} else {
-	print "[Info] ログインしています...\n";
-	$network->login();
+foreach(@ARGV){
+	if($_ =~ /--username=(.+)/i){
+		$user->{username} = $1;
+	} elsif ($_ =~ /--password=(.+)/i){
+		$user->{password} = $1;
+	}
 }
+
+# Connect
+eval{
+	connect_mc2wifi($user->{username}, $user->{password});
+}; if ($@) { die "[Error] $@\n"; }
 
 exit;
 
-sub connect {
+sub connect_mc2wifi {
+	my ($username, $password) = @_;
 
+	# Initialize the network module
+	my $network = Net::OECU::LAN::MC2Wifi->new(
+		username => $username,
+		password => $password,
+	);
+
+	if($network->is_logged_in()){
+		print "[Info] すでにログイン済みです。\n";
+	} else {
+		print "[Info] ログインしています...\n";
+		$network->login();
+		if($network->is_logged_in()){
+			print "[Info] ログイン成功 :)\n"
+		} else {
+			print "[Error] ログイン失敗 :(\n";
+		}
+	}
+	return;
 }
