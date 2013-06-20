f0d2138672222dc15c47d565679b2063eda33801
Masanori Ohgita <mp@ohgita.info>
2013-06-20 18:39:02 +0900

    Fix(install_ubuntu.sh)

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 #
-# OdenWlan perl install script for Ubuntu (10.xx or later)
-# 
+# OdenWlan perl install script for Ubuntu 10.xx or later
+# $ sh install_ubuntu.sh 
 
-echo "-- OdenWlan-perl Install script --"
+echo "-- OdenWlan-perl インストールスクリプト --"
 
-echo "Install dependency packages..."
-sudo apt-get install make perl cpan libssl-devel
+echo "依存モジュールをインストールしています..."
+sudo apt-get install make perl cpan libssl-dev
 sudo cpan App:cpanminus
 sudo LANG=C cpan Crypt::SSLeay
 
-echo "Install OdenWlan-perl..."
+echo "OdenWlan-perlをインストールしています..."
 perl Makefile.PL
 make
+IS_SUCCESS_MAKE=$?
+if [ $IS_SUCCESS_MAKE -eq 0 ]; then
+	echo "エラーが発生しました."
+	exit -1
+fi
+
 sudo make install
 
 echo "Done :)"
