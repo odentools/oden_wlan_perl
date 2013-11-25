a4578e50b8d3997d24cc8488b54f05967466c39d
Masanori Ohgita <mp@ohgita.info>
2013-11-25 09:01:38 +0900

    Fix: Install process for dependency cpan module

--- install_arch-linux.sh
+++ install_arch-linux.sh
@@ -8,6 +8,27 @@ echo "-- OdenWlan-perl インストールスクリプト --"
 PROXY_URL="http://172.25.250.41:8080"
 CON_NETWORK=1
 
+check_cmd() {
+        which $1 > /dev/null 2>&1
+        if [ $? -eq 0 ]; then
+                # Already installed
+                return 1
+        else
+                # Not installed
+                return 0
+        fi
+}
+
+# -----
+
+check_cmd 'wget'
+if [ $? -eq 0 ]; then
+	echo "[Error] wgetコマンドが見つかりません。"
+	echo "pacmanなどを用いて'wget'をインストールした後、再度実行してください。"
+	echo "$ sudo pacman -Syu wget"
+	exit 0
+fi
+
 echo "接続先ネットワークを識別しています..."
 wget http://google.com/ --no-proxy -O - 1>/dev/null 2>/dev/null
 IS_SUCCESS_CON=$?
@@ -32,9 +53,12 @@ if [ $CON_NETWORK -eq 1 ]; then
 	if [ -n "$PERL5LIB" ]; then
 		echo "CPAN modules instal to local directory..."
 		LANG=C cpanm Crypt::SSLeay
+		LANG=C cpanm --installdeps .
+
 	else
 		echo "CPAN modules instal to system..."
 		sudo LANG=C cpanm Crypt::SSLeay
+		sudo LANG=C cpanm --installdeps .
 	fi
 elif [ $CON_NETWORK -eq 2 ]; then
 	# If connected with MC2Wifi
@@ -44,9 +68,11 @@ elif [ $CON_NETWORK -eq 2 ]; then
 	if [ -n "$PERL5LIB" ]; then
 		echo "CPAN modules instal to local directory..."
 		http_proxy=$PROXY_URL LANG=C cpanm Crypt::SSLeay
+		http_proxy=$PROXY_URL LANG=C cpanm --installdeps .
 	else
 		echo "CPAN modules instal to system..."
 		sudo http_proxy=$PROXY_URL LANG=C cpanm Crypt::SSLeay
+		sudo http_proxy=$PROXY_URL LANG=C cpanm --installdeps .
 	fi
 fi
 
@@ -83,3 +109,5 @@ fi
 
 echo "インストール完了 :)"
 echo "Enjoy browsing! $ odenwlan"
+exit 0
+
